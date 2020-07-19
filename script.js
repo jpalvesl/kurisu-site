a2a5b9c5567e50096586f71bac72aacce8f511f8
João Lima <jpalvesl101@gmail.com>
2020-07-19 01:32:04 +0000

    criando seçao de comandos

--- script.js
+++ script.js
@@ -1,1 +1,177 @@
-console.log('testezin')
+const prefix = 'ks.'
+const commands = [
+  {
+    name: `${prefix}avatar`,
+    description: 'Retorna  os links das imagens das pessoas mencionadas',
+    usage: [
+      `${prefix}avatar - Mostra o link da pessoa usou o comando`,
+      `${prefix}avatar [@user] - Mostra os links das pessoas mencionadas`
+    ]
+  },
+  {
+    name: `${prefix}invite`,
+    description: 'Manda no chat uma mensagem com o link de adicionar a Kurisu ao servidor e alguns outros links importantes',
+    usage: [
+      `${prefix}invite`,
+    ]
+  },
+  {
+    name: `${prefix}roleinfo`,
+    description: 'Mostra informações sobre um determinado cargo do servidor',
+    usage: [
+      `${prefix}roleinfo <nome-do-cargo>`,
+    ]
+  },
+  {
+    name: `${prefix}serverinfo`,
+    description: 'Mostra informações sobre o servidor atual',
+    usage: [
+      `${prefix}serverinfo`,
+    ]
+  },
+  {
+    name: `${prefix}ping`,
+    description: 'Comando de interação com a Kurisu',
+    usage: [
+      `${prefix}ping`,
+    ]
+  },
+  {
+    name: `${prefix}beep`,
+    description: 'Mostra que você também sabe falar a lingua dos robôs, isso mesmo, nem todos usam binário pra se comunicar',
+    usage: [
+      `${prefix}beep`,
+    ]
+  },
+  {
+    name: `${prefix}ban`,
+    description: 'Comando pra botar medo nos usuários',
+    usage: [
+      `${prefix}ban @user`,
+    ]
+  },
+  {
+    name: `${prefix}hug`,
+    description: 'Abraça um outro usuário',
+    usage: [
+      `${prefix}hug @user`,
+    ]
+  },
+  {
+    name: `${prefix}kiss`,
+    description: 'Manda um beijinho pra outro usuário',
+    usage: [
+      `${prefix}kiss @user`,
+    ]
+  },
+  {
+    name: `${prefix}highfive`,
+    description: 'Faz um highfive com algum consagrado/consagrada do servidor',
+    usage: [
+      `${prefix}highfive @user`,
+    ]
+  },
+  {
+    name: `${prefix}corona`,
+    description: 'Mostra dados a cerca do COVID-19 no Brasil e no mundo',
+    usage: [
+      `${prefix}corona`,
+    ]
+  },
+  {
+    name: `${prefix}weather`,
+    description: 'Mostra dados de como está o tempo na cidade que você pesquisou',
+    usage: [
+      `${prefix}weather <nome-da-cidade>`,
+    ]
+  },
+  {
+    name: `${prefix}8ball`,
+    description: 'Simula o funcionamento da toda poderosa bola8',
+    usage: [
+      `${prefix}8ball <pergunta>`,
+    ]
+  },
+  {
+    name: `${prefix}search`,
+    description: 'Busca notícias e mostra no discord, com seus respectivos links',
+    usage: [
+      `${prefix}search - Mostra as manchetes`,
+      `${prefix}search <busca> - Mostra os resultados da busca, caso exita`,
+    ]
+  },
+  {
+    name: `${prefix}coinflip`,
+    description: 'Gira uma moeda uma ou mais vezes e mostra qual foi o resultado',
+    usage: [
+      `${prefix}coinflip - Gira a moeda uma vez`,
+      `${prefix}coinflip <vezes> - Gira a moeda x vezes`,
+    ]
+  },
+  {
+    name: `${prefix}roll`,
+    description: 'Rola um dado específico uma ou mais vezes e mostra os resultados e o total',
+    usage: [
+      `${prefix}roll <tipo-de-dado> <vezes>`,
+      `${prefix}roll - Rola o d6 uma vez`,
+      `${prefix}roll <tipo-de-dado> - Rola o tipo de dado apenas uma vez`,
+      `${prefix}roll <vezes> Rola o d6 x vezes`,
+    ]
+  },
+  {
+    name: `${prefix}random-number`,
+    description: 'Te mostra um número aleatório com base no intervalo que você decidiu',
+    usage: [
+      `${prefix}random-number <inicio-intervalo> <fim-intervalo>`,
+    ]
+  },
+  {
+    name: `${prefix}gif`,
+    description: 'Manda um gif',
+    usage: [
+      `${prefix}gif - Retorna um gif aleatório`,
+      `${prefix}gif <busca> - Retorna um gif com base no que você pesquisou`,
+    ]
+  },
+  {
+    name: `${prefix}filter`,
+    description: 'Aplica filtros em uma imagem e manda a imagem de volta pra o discord',
+    usage: [
+      `${prefix}filter <url> [<filtros>]`,
+      `${prefix}filter [<filtros>] - Adicione esse comando na descrição de uma imagem que você está enviando para o discord`,
+    ]
+  },
+]
+
+
+function addCommands() {
+  const list = document.querySelector('.list-commands')
+
+  commands.forEach(command => {
+    const commandElement = document.createElement('li')
+    commandElement.classList.add('command')
+
+    const commandName = document.createElement('strong')
+    commandName.innerText = command.name
+    
+    const rightDiv = document.createElement('div')
+    const descriptionElement = document.createElement('p')
+    descriptionElement.innerText = command.description
+    rightDiv.appendChild(descriptionElement)
+
+    const usageDiv = document.createElement('div')
+    command.usage.forEach(mode => {
+      const usageMode = document.createElement('p')
+      usageMode.innerText = mode
+      
+      usageDiv.appendChild(usageMode) 
+      rightDiv.appendChild(usageDiv)
+    })
+
+    commandElement.appendChild(commandName)
+    commandElement.appendChild(rightDiv)
+    list.appendChild(commandElement)
+  })
+}
+
+addCommands()
